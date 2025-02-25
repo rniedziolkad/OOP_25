2b6e8bd198a7a8fe701ee2a7b3064b447ac359ce
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2025-02-25 17:41:21 +0100

    dodano translate

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -3,9 +3,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         Point p1 = new Point();
-        p1.x = 5.0;
-        p1.y = 4.0;
+        p1.x = 30.0;
+        p1.y = 24.0;
 
-        System.out.println("P1: ("+p1.x+", "+p1.y+")");
+        System.out.println(p1);
+        System.out.println(p1.toSvg());
+
+        p1.translate(-6, 5);
+        System.out.println(p1);
+        Point tr = p1.translated(6, -5);
+        System.out.println(tr);
+        // taki sam jak tr
+        System.out.println(p1.translated(6, -5));
+
     }
 }
--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,4 +1,25 @@
 public class Point {
     public double x;
     public double y;
+
+    @Override
+    public String toString(){
+        return "("+x+", "+y+")";
+    }
+
+    public String toSvg(){
+        return "<circle r=\"5\" cx=\""+x+"\" cy=\""+y+"\" fill=\"black\" />";
+    }
+
+    public void translate(double dx, double dy){
+        this.x += dx;
+        y += dy;
+    }
+
+    public Point translated(double dx, double dy){
+        Point newPoint = new Point();
+        newPoint.x = x+dx;
+        newPoint.y = y+dy;
+        return newPoint;
+    }
 }
