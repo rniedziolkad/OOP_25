8f7671d14f69d0586ae4f6c4cb18e5cab12f67b1
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2025-02-25 16:53:52 +0100

    utworzony Point

---
+++ svg/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        Point p1 = new Point();
+        p1.x = 5.0;
+        p1.y = 4.0;
+
+        System.out.println("P1: ("+p1.x+", "+p1.y+")");
+    }
+}
