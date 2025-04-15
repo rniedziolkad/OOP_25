2ebfb065e943e3888d2d880126c9eb70ece3c071
student <student@mail.com>
2025-04-15 16:54:56 +0200

    dodano CustomList

---
+++ generyczne/src/CustomList.java
@@ -0,0 +1,117 @@
+import java.util.AbstractList;
+
+public class CustomList<E> extends AbstractList<E> {
+    private class Node{
+        E value;
+        Node next;
+
+        public Node(E value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public CustomList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public E get(int index) {
+        if (size <= index || index < 0) {
+            throw new IndexOutOfBoundsException("indeks poza zakresem [0, "+(size-1)+"]");
+        }
+        Node element = head;
+        for (int i=0; i<index; i++) {
+            element = element.next;
+        }
+        return element.value;
+    }
+
+    public boolean add(E e) {
+        addLast(e);
+        return true;
+    }
+
+    public void addLast(E value) {
+        Node newNode = new Node(value);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public E getLast() {
+        if (tail == null) {
+            throw new IllegalStateException("Lista jest pusta");
+        }
+        return tail.value;
+    }
+
+    public void addFirst(E value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public E getFirst() {
+        if (head == null) {
+            throw new IllegalStateException("Lista jest pusta");
+        }
+        return head.value;
+    }
+
+    public E removeFirst() {
+        if (head == null) {
+            throw new IllegalStateException("Lista jest pusta");
+        }
+        E value = head.value;
+        size--;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        return value;
+    }
+
+    public E removeLast() {
+        if (tail == null) {
+            throw new IllegalStateException("Lista jest pusta");
+        }
+        E value = tail.value;
+        size--;
+
+        if(tail == head) {
+            // 1 element, wyczyść listę
+            head = tail = null;
+            return value;
+        }
+
+        Node curr = head;
+        while (curr.next != tail) {
+            curr = curr.next;
+        }
+        curr.next = null;
+        tail = curr;
+        return value;
+    }
+
+
+}
--- generyczne/src/Main.java
+++ generyczne/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        CustomList<Integer> list = new CustomList<>();
+//        list.getFirst();
+//        list.getLast();
+        list.addFirst(1);
+        list.addLast(2);
+        list.addFirst(0);
+        list.add(3);
+
+        list.addFirst(1000);
+        list.addLast(-10);
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println("usuwam: "+list.removeFirst());
+        System.out.println("usuwam: "+list.removeLast());
+        System.out.println("first: "+list.getFirst());
+        System.out.println("last: "+list.getLast());
+        System.out.println("rozmiar: "+list.size());
+        System.out.println("list[0] = "+list.get(0));
+        System.out.println("list[3] = "+list.get(3));
+//        System.out.println("list[4] = "+list.get(4));
+        System.out.println("list[2] = "+list.get(2));
+//        System.out.println("list[-1] = "+list.get(-1));
+
     }
 }
